a6753f7128c2e23ca41879b554a922aec57f967f
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-03 23:51:13 +0200

    Updates

---
+++ precache-manifest.7fe5b6517804f886af6c92a1e50726de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d7ae4ba3bb74b84a0ce560fa1c70f5",
+    "url": "/weatherAPI/index.html"
+  },
+  {
+    "revision": "ec8320f2169517b58fcf",
+    "url": "/weatherAPI/static/css/main.586a8c9e.chunk.css"
+  },
+  {
+    "revision": "a1fa0f4d0febf6ec8fed",
+    "url": "/weatherAPI/static/js/2.529d526f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherAPI/static/js/2.529d526f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8320f2169517b58fcf",
+    "url": "/weatherAPI/static/js/main.ca623971.chunk.js"
+  },
+  {
+    "revision": "6f0a25eed778cc174d25",
+    "url": "/weatherAPI/static/js/runtime-main.185ea6f5.js"
+  }
+]);
